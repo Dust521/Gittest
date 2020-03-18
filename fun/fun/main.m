b866bd0f8effa3facea8c56f60d1a68549c93f33
{dongliang} <dongliang.smart@bytedance.com>
2020-03-18 23:00:31 +0800

    first commit

--- fun/fun/main.m
+++ fun/fun/main.m
@@ -12,6 +12,9 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        //I am from desktop!
+        
     }
     return 0;
 }
