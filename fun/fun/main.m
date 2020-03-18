555e4126ac8de044514bfef38f457288d1566992
董亮 <dongliang@dongliangdeMacBook-Pro.local>
2020-03-18 22:48:07 +0800

    first

---
+++ fun/fun/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  fun
+//
+//  Created by 董亮 on 2020/3/18.
+//  Copyright © 2020 董亮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
